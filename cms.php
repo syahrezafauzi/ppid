47f944b7ec77a6aaa8d0ed18fb13b28c8350b3c2
syahrezafauzi <sahzauzi@gmail.com>
2025-07-23 06:38:23 +0700

    checkpoint

--- cms.php
+++ cms.php
@@ -58,17 +58,65 @@
                         <div class="single_page_area">
                             <?php
                             $page = $_GET['page'];
-                            $data = file_get_contents("http://localhost:1347/api/menus?filters[page][url][\$eq]=". $page . "&populate[page][filters][url][\$eq]=" . $page);
-                            $data = json_decode($data)->data[0];
-                            $content = $data->page[0]->content;
-                            $image = @$data->page[0]->image;
-                            $pdf = @$data->page[0]->pdf;
-                            $title = $data->page[0]->title;
+                            $data = file_get_contents("http://localhost:1347/api/menus/view?url=" . $page);
+                            $data = json_decode($data);
+                            if ($data != null) {
+                                $content = $data[0]->page[0]->content ?? "";
+                                $image = @$data[0]->page[0]->image;
+                                $pdf = @$data[0]->page[0]->pdf;
+                                $title = $data[0]->page[0]->title;
+                            }
+
                             ?>
                             <h2 style="color:#000000;" class="post_titile"><?= $title ?></h2>
                             <div class="single_page_content">
                                 <?= $content ?>
                             </div>
+                            <?php
+                            if ($pdf) {
+                                foreach ($pdf as $key => $value) {
+                                    $title = $value->title;
+                                    $file = $value->file;
+                                    ?>
+                                    <div class="single_page_content">
+                                        <p><?= $title ?><br />
+                                        </p>
+                                        <table id="myTable">
+                                            <?php
+                                            foreach ($file as $key => $value) {
+                                                ?>
+                                                <tr>
+                                                    <td><?= $value->name ?></td>
+                                                    <td><a href="http://localhost:1347<?= $value->src ?>"><i class="fa fa-eye"
+                                                                aria-hidden="true"></i></a></td>
+                                                </tr>
+                                                <?php
+                                            } ?>
+                                        </table>
+                                    </div>
+                                    <?php
+                                }
+
+                                ?>
+
+                                <?php
+                            } ?>
+                            <?php
+                            if ($image) {
+                                ?>
+                                <div class="single_page_content">
+                                    <?php
+                                    foreach ($image as $key => $value) {
+                                        ?>
+                                        <img src="http://localhost:1347<?= $value ?>">
+                                        <?php
+                                    }
+                                    ?>
+                                </div>
+                                <?php
+
+                            }
+                            ?>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-3 col-sm-3">
